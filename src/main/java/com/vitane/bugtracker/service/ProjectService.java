e669975f8544d5df66fa054afbcfdd6c9cfd04bd
vitaly <vitaly.nec@gmail.com>
2019-09-23 11:48:59 +0700

    fix unused import

--- src/main/java/com/vitane/bugtracker/service/ProjectService.java
+++ src/main/java/com/vitane/bugtracker/service/ProjectService.java
@@ -4,7 +4,6 @@ import com.vitane.bugtracker.entity.Project;
 import com.vitane.bugtracker.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
