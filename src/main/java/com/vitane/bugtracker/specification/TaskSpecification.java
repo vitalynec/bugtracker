c61f982d97cd3d798534dbb213d147f8119c84d1
vitaly <vitaly.nec@gmail.com>
2019-09-23 11:43:02 +0700

    fix warning

--- src/main/java/com/vitane/bugtracker/specification/TaskSpecification.java
+++ src/main/java/com/vitane/bugtracker/specification/TaskSpecification.java
@@ -4,36 +4,27 @@ import com.vitane.bugtracker.entity.Status;
 import com.vitane.bugtracker.entity.Task;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.*;
 import java.time.LocalDateTime;
 
 public class TaskSpecification {
 
     public static Specification<Task> getTasksByStatus(Status status) {
-        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.equal(root.get("status"), status);
-            return predicate;
-        };
+        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("status"), status);
     }
 
     public static Specification<Task> getTasksByPriority(int priority) {
-        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.equal(root.get("priority"), priority);
-            return predicate;
-        };
+        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("priority"), priority);
     }
 
     public static Specification<Task> getTasksByDateTo(LocalDateTime dateTo) {
-        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.lessThanOrEqualTo(root.get("dateOfCreation"), dateTo);
-            return predicate;
-        };
+        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.lessThanOrEqualTo(root.get("dateOfCreation"), dateTo);
     }
 
     public static Specification<Task> getTasksByDateFrom(LocalDateTime dateFrom) {
-        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.greaterThanOrEqualTo(root.get("dateOfCreation"), dateFrom);
-            return predicate;
-        };
+        return (Specification<Task>) (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.greaterThanOrEqualTo(root.get("dateOfCreation"), dateFrom);
     }
 }
