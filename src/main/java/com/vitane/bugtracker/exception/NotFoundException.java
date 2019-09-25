9384e6b0879b59fc2c3fcd9e9d9ac1ce8e38715c
vitaly <vitaly.nec@gmail.com>
2019-09-25 12:04:56 +0700

    add exceptions package

---
+++ src/main/java/com/vitane/bugtracker/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.vitane.bugtracker.exception;
+
+public class NotFoundException extends Exception {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
